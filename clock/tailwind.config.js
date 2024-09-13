0a9b6014d9cefe2ec02614eb65bbb642b35aaec1
AADI GUPTA <aadigupta2007@yahoo.com>
2024-09-13 20:59:53 +0530

    assignments of 12th and 13th sep(first commit)

---
+++ clock/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./style.css"], // Ensure your HTML and CSS files are included
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
